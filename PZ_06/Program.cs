ddef6d4262fa258fa327a15d7784277f59f0bc60
Наталья Свиридова <sviridovanatala211@gmail.com>
2023-09-27 13:20:40 +0500

    Добавлена 6 практика

---
+++ PZ_06/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace PZ_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[20];
+            Random random = new Random();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(1, 101);
+            }
+
+            Console.WriteLine("Все элементы массива:");
+            foreach (int num in array)
+            {
+                Console.Write(num + " ");
+            }
+
+            Console.WriteLine("\nУникальные элементы массива:");
+            foreach (int number in array)
+            {
+                int count = 0;
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (number == array[i])
+                    {
+                        count++;
+                    }
+                }
+
+                if (count == 1)
+                {
+                    Console.Write($"{number} ");
+                }
+            }
+
+        }
+    }
+}
