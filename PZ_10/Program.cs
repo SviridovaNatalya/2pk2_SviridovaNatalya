74ccf8263b0b0b47efb340ba4eeddf8011b5a86e
Наталья Свиридова <sviridovanatala211@gmail.com>
2023-11-25 20:49:11 +0500

    Добавлена 10  практика

---
+++ PZ_10/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace PZ_10
+{
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Введите произвольный текст:");
+                string text = Console.ReadLine();
+
+                string[] emails = text.Split(' ');
+                foreach (string email in emails)
+                {
+                    if (IsValidEmail(email))
+                    {
+                        string userName = GetUserName(email);
+                        Console.WriteLine($"Привет, {userName}!");
+                    }
+                }
+            }
+
+            static bool IsValidEmail(string email)
+            {
+                return email.Contains("@"); // Проверяем наличие символа @ в строке
+            }
+
+            static string GetUserName(string email)
+            {
+                int atIndex = email.IndexOf("@");
+                return email.Substring(0, atIndex);
+            }
+        }
+    }
