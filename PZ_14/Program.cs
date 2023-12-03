99e1882537cec29d23bc2df1142d8cf831d504df
Наталья Свиридова <sviridovanatala211@gmail.com>
2023-12-03 13:00:50 +0500

    Добавлена 14 практика.

---
+++ PZ_14/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace PZ_14
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                Console.WriteLine("Введите путь к папке:");
+                string folderPath = Console.ReadLine();
+
+                // Проверяем существует ли указанная папка
+                if (Directory.Exists(folderPath))
+                {
+                    // Получаем список текстовых файлов в указанной папке
+                    string[] files = Directory.GetFiles(folderPath, "*.txt");
+
+                    if (files.Length > 0)
+                    {
+                        // Создаем файл OUT.TXT в текущей директории
+                        string outputPath = Path.Combine(Directory.GetCurrentDirectory(), "OUT.TXT");
+
+                        // Используем потоки для чтения файлов и записи в новый файл
+                        using (var outputStream = new StreamWriter(outputPath))
+                        {
+                            foreach (string file in files)
+                            {
+                                // Читаем содержимое каждого текстового файла
+                                using (var inputStream = new StreamReader(file))
+                                {
+                                    string content = inputStream.ReadToEnd();
+                                    // Записываем содержимое в OUT.TXT
+                                    outputStream.WriteLine(file + ":\n" + content);
+                                }
+                            }
+                        }
+
+                        Console.WriteLine("Содержимое текстовых файлов успешно скопировано в OUT.TXT");
+                    }
+                    else
+                    {
+                        Console.WriteLine("В указанной папке отсутствуют текстовые документы");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Указанная папка не существует");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка: " + ex.Message);
+            }
+        }
+    }
+}
